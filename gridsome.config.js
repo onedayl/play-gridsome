945f9927296049f63cf779d75503246eb8f03c22
onedayl <onedayl@Daisy>
2020-04-29 23:48:06 +0800

    remove github pages deploy config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   siteName: '良禽佳木&木匠小强官网 ｜ 实木定制家具品牌',
   siteDescription: '良禽佳木由木匠世家创立，秉持“真材实料好工艺”的产品理念，从原料着手，自建工厂，用好原料、好工艺做禁得住用，耐得住看的现代实木家具。',
-  siteUrl: 'https://onedayl.github.io',
-  pathPrefix: '/play-gridsome',
   titleTemplate: '良禽佳木&木匠小强官网',
   icon: './src/assets/favicon.png',
   plugins: [{
